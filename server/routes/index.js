06415bac4e568cdee6f83ff1aa8b8d5f7a557d11
Gabriel <grodriguez@emergya.com>
2020-04-24 13:30:34 +0200

    feat(routes): Move root routes to index file

---
+++ server/routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const users = require('./users.routes');
+const movies = require('./movies.routes');
+
+router.use('/users', users);
+router.use('/movies', movies);
