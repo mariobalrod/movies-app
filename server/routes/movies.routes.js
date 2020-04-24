60a91528b2740a48aedc69c09200ec57c75ef425
Gabriel <grodriguez@emergya.com>
2020-04-24 13:43:18 +0200

    style(): Delete obvious comments

--- server/routes/movies.routes.js
+++ server/routes/movies.routes.js
@@ -5,15 +5,10 @@ const {
     getAllMovies, getMovieById, createMovie, updateMovie, deleteMovie
 } = require('../controllers/movies.controllers');
 
-// Obtener todos las peliculas
 router.get('/', getAllMovies);
-// Obtener Una Pelicula por id
 router.get('/:id', getMovieById);
-// Crear Pelicula
 router.post('/', createMovie);
-// Modificar Pelicula
 router.put('/:id', updateMovie);
-// Eliminar Pelicula
 router.delete('/:id', deleteMovie);
 
 module.exports = router;
