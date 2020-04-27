91a405c4510c81be3ead7a466e031391155b0503
mariobalrod <marioballestero.rdg@gmail.com>
2020-04-27 20:09:23 +0200

    style : improving MovieNavMenu style

--- client/src/components/movies/MovieNavMenu.js
+++ client/src/components/movies/MovieNavMenu.js
@@ -15,38 +15,62 @@ const MovieNavMenu = (props) => {
 
     return (
         <div style={{width: 250}}>
-            <Nav className="navigation">
-
-                <Nav.Item>
-                    <Nav.Link href="#" onClick={() => addMovie(movie_id, user_id, 'pendiente')}>
-                        <VisibilityIcon style={{ fontSize: 20 }} className="linkList"/>
-                    </Nav.Link>
-                </Nav.Item >
-
-                <Nav.Item>
-                    <Nav.Link href="#" onClick={() => addMovie(movie_id, user_id, 'vista')} >
-                        <VisibilityOffIcon style={{ fontSize: 20 }} className="linkList" />
-                    </Nav.Link>
-                </Nav.Item>
-
-                <Nav.Item>
-                    <Nav.Link href="#" onClick={() => addMovie(movie_id, user_id, 'favorita')} >
-                        <StarIcon style={{ fontSize: 20 }} className="linkList" />
-                    </Nav.Link>
-                </Nav.Item>
                 {
                     (props.owner)
                     ? (
-                        <Nav.Item>
-                            <Nav.Link href="#" onClick={() => deleteMovie(movie_id, user_id)} >
-                                <DeleteForeverIcon className="delete" style={{ fontSize: 20 }} />
-                            </Nav.Link>
-                        </Nav.Item>
-                    ) : ''
+                        <Nav>
+                            <Nav.Item>
+                                <Nav.Link href="#" onClick={() => addMovie(movie_id, user_id, 'pendiente')}>
+                                    <VisibilityIcon style={{ fontSize: 20 }} className="linkList"/>
+                                </Nav.Link>
+                            </Nav.Item >
+
+                            <Nav.Item>
+                                <Nav.Link href="#" onClick={() => addMovie(movie_id, user_id, 'vista')} >
+                                    <VisibilityOffIcon style={{ fontSize: 20 }} className="linkList" />
+                                </Nav.Link>
+                            </Nav.Item>
+
+                            <Nav.Item>
+                                <Nav.Link href="#" onClick={() => addMovie(movie_id, user_id, 'favorita')} >
+                                    <StarIcon style={{ fontSize: 20 }} className="linkList" />
+                                </Nav.Link>
+                            </Nav.Item>
+
+                            <Nav.Item>
+                                <Nav.Link href="#" onClick={() => deleteMovie(movie_id, user_id)} >
+                                    <DeleteForeverIcon className="delete" style={{ fontSize: 20 }} />
+                                </Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    ) : (
+
+                        <Nav className="navigation">
+
+                            <Nav.Item>
+                                <Nav.Link href="#" onClick={() => addMovie(movie_id, user_id, 'pendiente')}>
+                                    <VisibilityIcon style={{ fontSize: 20 }} className="linkList"/>
+                                </Nav.Link>
+                            </Nav.Item >
+
+                            <Nav.Item>
+                                <Nav.Link href="#" onClick={() => addMovie(movie_id, user_id, 'vista')} >
+                                    <VisibilityOffIcon style={{ fontSize: 20 }} className="linkList" />
+                                </Nav.Link>
+                            </Nav.Item>
+
+                            <Nav.Item>
+                                <Nav.Link href="#" onClick={() => addMovie(movie_id, user_id, 'favorita')} >
+                                    <StarIcon style={{ fontSize: 20 }} className="linkList" />
+                                </Nav.Link>
+                            </Nav.Item>
+
+                        </Nav>
+                        
+                    )
                 }
                 
 
-            </Nav>
         </div>
     );
 }
