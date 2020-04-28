52078d1dd92c719692c7360e29069055db493514
mariobalrod <marioballestero.rdg@gmail.com>
2020-04-28 13:13:19 +0200

    feat : necessary props for overview card

--- client/src/components/movies/MovieOverviewCard.js
+++ client/src/components/movies/MovieOverviewCard.js
@@ -5,6 +5,13 @@ const MovieOverwiewCard = (props) => {
         <div>
             <h1>Card Overview</h1>
             <h4>{props.id}</h4>
+            <h4>{props.poster_path}</h4>
+            <h4>{props.title}</h4>
+            <h4>{props.overview}</h4>
+            <h4>{props.vote_average}</h4>
+            <h4>{props.homepage}</h4>
+            <h4>{props.popularity}</h4>
+            <h4>{props.vote_count}</h4>
         </div>
     );
 }
--- client/src/views/MovieDetails.js
+++ client/src/views/MovieDetails.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+import { fetchMovieById } from '../helpers/tmdbConfig';
 
 import MovieOverviewCard from '../components/movies/MovieOverviewCard';
 
 const MovieDetails = (props) => {
 
+    const [movie, setMovie] = useState({});
+
+    useEffect(() => {
+        const id_movie = props.match.params.id;
+        fetchMovie(id_movie);
+    }, [])
+
+    const fetchMovie = async (id_movie) => {
+        const movieF = await fetchMovieById(id_movie);
+        setMovie(movieF);
+    }
+
     return ( 
         <div>
             <h1>Overview</h1>
-            <MovieOverviewCard id={props.match.params.id}/>
-            <MovieOverviewCard />
-            <MovieOverviewCard />
+            <MovieOverviewCard 
+                id={props.match.params.id} 
+                poster_path={movie.path} 
+                title={movie.title} 
+                overview={movie.overview} 
+                vote_average={movie.vote_average} 
+                vote_count={movie.vote_count} 
+                homepage={movie.homepage} 
+                popularity={movie.popularity}
+            />
         </div>
     );
 
