2682146b8b8c34702b66012b62bb0aa2c060ff76
mariobalrod <marioballestero.rdg@gmail.com>
2020-05-21 11:24:34 +0200

    feat : delete action for lists added

--- client/src/components/lists/ListCard.js
+++ client/src/components/lists/ListCard.js
@@ -2,24 +2,32 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+//Actions
+import { deleteList } from '../../helpers/listsActions';
+
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import TheatersRoundedIcon from '@material-ui/icons/TheatersRounded';
 
 const ListCard = (props) => {
 
+    const id = props.list._id;
     const name = props.list.name;
     const description = props.list.description;
 
     return (
-        <Link to={`/lists/${name}`} className="linkList">
-            <Card className="animated flipInY" style={{ width: 300, margin: 50 }}>
-                <DeleteForeverIcon className="deleteList" />
-                <Card.Body>
-                    <TheatersRoundedIcon className="mx-auto" style={{ fontSize: 250 }} />
-                </Card.Body>
-                <Card.Footer className="text-muted" style={{ textAlign: "center" }}>{name}</Card.Footer>
-            </Card>
-        </Link>
+        <div className="custom">
+            <div className="mx-auto mb-2" style={{width: 30}}>
+                <DeleteForeverIcon className="deleteList" onClick={() => deleteList(id)}/>
+            </div>
+            <Link to={`/lists/${name}`} className="linkList">
+                <Card className="animated flipInY" style={{ width: 300, margin: 50, marginTop: 0 }}>
+                    <Card.Body>
+                        <TheatersRoundedIcon className="mx-auto" style={{ fontSize: 250 }} />
+                    </Card.Body>
+                    <Card.Footer className="text-muted" style={{ textAlign: "center" }}>{name}</Card.Footer>
+                </Card>
+            </Link>
+        </div>
     )
 }
 
